88574986c5c02296dd7a5a157e85b7f6ebe6d794
wangshichao <shichao.w@outlook.com>
2023-04-20 16:06:04 +0800

    [style] rename params

--- src/scouttwang/flatdict/__init__.py
+++ src/scouttwang/flatdict/__init__.py
@@ -16,17 +16,17 @@ def flatdict_to_hierarchical(flatdict: Mapping[str, Any]) -> Dict[str, Any]:
     return kwargs
 
 
-def flatten_dict(dict: Mapping[str, Any]) -> Dict[str, Any]:
-    def _inner_flatten(kwargs: Mapping[str, Any], prefix: str = ""):
+def flatten_dict(kwargs: Mapping[str, Any]) -> Dict[str, Any]:
+    def _inner_flatten(_kwargs: Mapping[str, Any], prefix: str = ""):
         flatdict: Dict[str, Any] = {}
-        for key, value in kwargs.items():
+        for key, value in _kwargs.items():
             if isinstance(value, Mapping):
-                flatdict.update(flatten_dict(value, f"{prefix+key}."))
+                flatdict.update(_inner_flatten(value, f"{prefix+key}."))
             else:
                 flatdict[prefix + key] = value
         return flatdict
 
-    return _inner_flatten(dict)
+    return _inner_flatten(kwargs)
 
 
 __all__ = ["flatdict_to_hierarchical", "flatten_dict"]
